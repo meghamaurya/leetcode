baff1c0310391e4a1ff4b16f499c430f0cf55eee
Megha Maurya <104269540+meghamaurya@users.noreply.github.com>
2022-09-11 13:32:11 +0530

    Time: 138 ms (32.15%), Space: 52.8 MB (7.59%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,11 +13,7 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if(p.val < root.val && q.val < root.val) {
-        return lowestCommonAncestor(root.left, p, q)
-    };
-    if(p.val > root.val && q.val > root.val) {
-        return lowestCommonAncestor(root.right, p, q)
-    };
+    if(p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q)
+    if(p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q)
     return root;
 };
