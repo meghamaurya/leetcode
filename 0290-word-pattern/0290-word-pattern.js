c5cc64ede5e849dded618180a4baed4c2f67d166
Megha Maurya <104269540+meghamaurya@users.noreply.github.com>
2023-02-19 23:22:16 +0530

    Time: 66 ms (52.64%), Space: 41.6 MB (72.16%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,13 +4,14 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-    s = s.split(' ')   
-
-    if (s.length != pattern.length) return false   
-
-    for (let i = 0; i < s.length; i++) 
-        if (s.indexOf(s[i]) != pattern.indexOf(pattern[i]))    
-            return false        
+    s = s.split(" ");
     
-    return true 
+    if(s.length != pattern.length) return false;
+    
+    for(let i = 0; i < s.length; i++)
+        if(s.indexOf(s[i]) != pattern.indexOf(pattern[i]))
+            return false;
+        
+        return true;
+    
 };
